bf17a46a49eb8751022a50f67f22af7677d01638
imanimonae1 <imsappington@cps.edu>
2024-11-21 16:38:37 +0000

    3rd

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let ghostSchool=document.querySelector('.ghostSchool');
 let hauntedSchool=document.querySelector('.hauntedSchool');
 let text=document.querySelector('p');
 hauntedSchool.style.display="none";
-
+let darkHallway=document.querySelector('.darkHallway')
+darkhallway.style.display="none";
 left.addEventListener('click',function(){
 
     ghostSchool.style.display="none";
@@ -19,4 +20,6 @@ hauntedSchool.addEventListener('dblclick',function(){
     text.innerHTML="Let's play";
     
     
+    
     });
+
