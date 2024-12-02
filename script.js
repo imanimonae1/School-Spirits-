128c8c2f62988cce8ecf7a08188bb318d7aa8197
imanimonae1 <imsappington@cps.edu>
2024-12-02 16:54:29 +0000

    7

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ hauntedSchool.style.display="none";
 let darkHallway=document.querySelector('.darkHallway');
 darkHallway.style.display="none";
 let school=document.querySelector('.school')
+school.style.display="none";
 let exit=document.querySelector('.exit');
 exit.style.display="none";
 let hauntedClassroom=document.querySelector('.hauntedClassroom')
-hauntedClassroom.sty;exit.display="none";
+hauntedClassroom.style.display="none";
+let schoolLunchroom=document.querySelector('.schoolLunchroom')
+schoolLunchroom.style.display="none";
+let pumpkin=document.querySelector('.pumpkin');
+pumpkin.style.display="none";
 //left button 
 left.addEventListener('click',function(){
 
@@ -18,7 +23,9 @@ left.addEventListener('click',function(){
     left.style.display="none";
     right.style.display="none";
     hauntedSchool.style.display="block";
-    
+    hauntedClassroom.style.display="none";
+    schoolLunchroom.style.display="none";
+    pumpkin.style.display="none";
 
     text.innerHTML="You are trapped in an old school filled with a bunch of school spirits. How will you find your way out? Will you make it out alive or will you become one of the school spirits? Double click to enter.";
 });
@@ -27,8 +34,10 @@ hauntedSchool.addEventListener('dblclick',function(){
     text.innerHTML="Let's play";
     hauntedSchool.style.display="none";
     darkHallway.style.display="block";
-    text.innerHTML="Go down the dark school hallway and try to find your way out. ";
-    
+    hauntedClassroom.style.display="none";
+    text.innerHTML="Go down the dark school hallway and try to find your way out. Find the school emoji to find your way out. ";
+    schoolLunchroom.style.display="none";
+    school.style.display="block";
     });
 
 //mouse eneter event
@@ -36,10 +45,26 @@ school.addEventListener('mouseenter',function(){
 darkHallway.style.display="none"
 text.innerHTML="You have safely exited the school!";
 exit.style.display="block";
+hauntedClassroom.style.display="none";
 });
 
 //right button
 right.addEventListener('click',function(){
 hauntedClassroom.style.display="block";
+ghostSchool.style.display="none";
+hauntedSchool.style.display="none";
+exit.style.display="none";
+school.style.display="none";
+text.innerHTML="You get locked in one of the classrooms. Hover over the pumpkin to go to the next place.";
+schoolLunchroom.style.display="none";
+pumpkin.style.display="block";
+left.style.display="none";
+right.style.display="none";
+});
+//mouse enter event 2
+pumpkin.addEventListener('mouseenter',function(){
+hauntedClassroom.style.display="none";
+text.innerHTML=" You never find your way out!";
+schoolLunchroom.style.display="block";
 
 });
